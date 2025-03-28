bd516d22b287a59a4c555bef4425ced366303aea
juand011 <juand.ocampom@uqvirtual.edu.co>
2025-03-27 20:44:27 -0500

    Actualizacion Juan David Ocampo

---
+++ src/com/laboratorio/Miclase.java
@@ -0,0 +1,7 @@
+package com.laboratorio;
+
+public class Miclase { 
+    public Miclase() { // El constructor ahora coincide con el nombre de la clase
+        System.out.println("Mensaje desde el constructor de la clase Juan David Ocampo ");
+    }
+}
